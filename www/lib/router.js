0ce7bdcddb5463aa3cd0e1310d54f0c9b4bf50c5
akhileshrai <gibbet@gmail.com>
2014-08-08 16:09:08 +0530

    Some comments added

--- www/lib/router.js
+++ www/lib/router.js
@@ -17,19 +17,25 @@ var router = (function () {
         var path = window.location.hash.substr(1),
             parts = path.split('/'),
             partsLength = parts.length;
-
-        for (var i = 0; i < routes.length; i++) {
+        console.log(path);
+		console.log(parts);
+		console.log("length: "+parts.length+"number of routes: "+routes.length);
+		
+        for (var i = 0; i < routes.length; i++) { //traverses through all routes
             var route = routes[i];
             if (route.parts.length === partsLength) {
                 var params = [];
                 for (var j = 0; j < partsLength; j++) {
                     if (route.parts[j].substr(0, 1) === ':') {
                         params.push(parts[j]);
+                        console.log("params");
+                        console.log(params);
                     } else if (route.parts[j] !== parts[j]) {
                         break;
                     }
                 }
                 if (j === partsLength) {
+                	console.log("calling handler" + route.handler + " with this" + undefined + " and params: " + params);
                     route.handler.apply(undefined, params);
                     return;
                 }
